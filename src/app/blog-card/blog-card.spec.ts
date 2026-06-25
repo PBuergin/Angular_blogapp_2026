7abad5582897b5fdc716ffee871cfd123d3eacce
Paul Buergin <paul.buergin@hftm.ch>
2026-06-25 16:20:59 +0200

    fix: unit tests

--- src/app/blog-card/blog-card.spec.ts
+++ src/app/blog-card/blog-card.spec.ts
@@ -1,10 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BlogCardComponent } from './blog-card';
+import { Blog } from '../../models/blog';
 
-describe('BlogCard', () => {
+describe('BlogCardComponent', () => {
   let component: BlogCardComponent;
   let fixture: ComponentFixture<BlogCardComponent>;
+
+  const mockBlog: Blog = {
+    id: 1,
+    title: 'Test Blog',
+    contentPreview: 'Test Preview',
+    author: 'Test Author',
+    likes: 0,
+    comments: 0,
+    likedByMe: false,
+    createdByMe: false,
+    createdAt: '2026-01-01T00:00:00',
+    updatedAt: '2026-01-01T00:00:00',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,8 +26,10 @@ describe('BlogCard', () => {
 
     fixture = TestBed.createComponent(BlogCardComponent);
 
+    fixture.componentRef.setInput('model', mockBlog);
+
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
