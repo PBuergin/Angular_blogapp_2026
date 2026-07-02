d2feec3350d27775bf9020614f3963e42dd759ac
Paul Buergin <paul.buergin@hftm.ch>
2026-07-02 22:34:32 +0200

    feat: implement material theming and dark mode

--- src/app/feature/blog/blog-overview-page/blog-overview-page.ts
+++ src/app/feature/blog/blog-overview-page/blog-overview-page.ts
@@ -2,18 +2,37 @@ import { Component, inject } from '@angular/core';
 import { BlogCardComponent } from '../blog-card/blog-card';
 import { Blog } from '../../../../models/blog';
 import { BlogService } from '../blog.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-blog-overview-page',
   standalone: true,
-  imports: [BlogCardComponent],
+  imports: [BlogCardComponent, MatButtonModule, MatIconModule],
   templateUrl: './blog-overview-page.html',
   styleUrl: './blog-overview-page.scss',
 })
 export class BlogOverviewPageComponent {
+  isDarkMode = false;
+
   private blogService = inject(BlogService);
 
   blogs: Blog[] = this.blogService.getAll();
+
+  constructor() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+    } else if (savedTheme === 'light') {
+      this.isDarkMode = false;
+    } else {
+      // Kein gespeichertes Theme → System-Einstellung verwenden
+      this.isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
+
+    document.documentElement.classList.toggle('dark-theme', this.isDarkMode);
+  }
 
   toggleLike(id: number): void {
     const blog = this.blogs.find((b) => b.id === id);
@@ -29,5 +48,14 @@ export class BlogOverviewPageComponent {
       blog.likedByMe = true;
       blog.likes++;
     }
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+
+    document.documentElement.classList.toggle('dark-theme', this.isDarkMode);
+
+    // Theme speichern
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
   }
 }
