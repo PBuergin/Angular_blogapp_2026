7bd6ce57925b7b2b86f26365e22a17cb69876c25
Paul Buergin <paul.buergin@hftm.ch>
2026-09-17 19:32:09 +0200

    feat: aufgabe 3

--- src/app/feature/blog-create/blog-create.ts
+++ src/app/feature/blog-create/blog-create.ts
@@ -1,5 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField, maxLength, minLength, required, submit } from '@angular/forms/signals';
+import {
+  form,
+  FormField,
+  maxLength,
+  minLength,
+  required,
+  submit,
+  validate,
+} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-blog-create',
@@ -20,12 +28,43 @@ export class BlogCreate {
     minLength(s.title, 3, { message: 'Titel muss mindestens 3 Zeichen lang sein' });
     maxLength(s.title, 100, { message: 'Titel darf maximal 100 Zeichen lang sein' });
 
+    // aufgabe 3a: custom title validator
+    validate(s.title, ({ value }) => {
+      const title = value();
+      // Regex Pattern für erlaubte Zeichen
+      const pattern = /^[A-Za-zÄÖÜäöüß0-9 ]+$/;
+
+      if (!pattern.test(title)) {
+        return {
+          kind: 'invalidCharacters',
+          message: 'Titel darf nur Buchstaben, Zahlen und Leerzeichen enthalten',
+        };
+      }
+
+      return null;
+    });
+
     // content validator
     required(s.content, { message: 'Inhalt ist erforderlich' });
     minLength(s.content, 10, { message: 'Inhalt muss mindestens 10 Zeichen lang sein' });
 
     // category validator
     required(s.category, { message: 'Kategorie ist erforderlich' });
+
+    // aufgabe 3b: Cross-Field validator
+    validate(s.content, ({ value, valueOf }) => {
+      const content = value();
+      const title = valueOf(s.title);
+
+      if (content.length < title.length * 2) {
+        return {
+          kind: 'contentTooShort',
+          message: 'Inhalt muss mindestens doppelt so lang wie der Titel sein',
+        };
+      }
+
+      return null;
+    });
   });
 
   onSubmit(event: Event) {
