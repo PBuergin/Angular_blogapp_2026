3cde610752a55fa901ea47a62a576631b5f989f1
Paul Buergin <paul.buergin@hftm.ch>
2026-09-17 19:11:02 +0200

    feat: aufgabe2

--- src/app/feature/blog-create/blog-create.ts
+++ src/app/feature/blog-create/blog-create.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, maxLength, minLength, required, submit } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-blog-create',
@@ -14,10 +14,25 @@ export class BlogCreate {
     category: 'general',
   });
 
-  blogForm = form(this.blogModel);
+  blogForm = form(this.blogModel, (s) => {
+    // title validator
+    required(s.title, { message: 'Titel ist erforderlich' });
+    minLength(s.title, 3, { message: 'Titel muss mindestens 3 Zeichen lang sein' });
+    maxLength(s.title, 100, { message: 'Titel darf maximal 100 Zeichen lang sein' });
+
+    // content validator
+    required(s.content, { message: 'Inhalt ist erforderlich' });
+    minLength(s.content, 10, { message: 'Inhalt muss mindestens 10 Zeichen lang sein' });
+
+    // category validator
+    required(s.category, { message: 'Kategorie ist erforderlich' });
+  });
 
   onSubmit(event: Event) {
     event.preventDefault();
-    console.log(this.blogModel());
+
+    submit(this.blogForm, async () => {
+      console.log(this.blogModel());
+    });
   }
 }
